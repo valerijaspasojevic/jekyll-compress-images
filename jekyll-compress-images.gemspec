4435f125855a90a7daad009b5a97293bd074c507
Valerija Spasojevic <spasojevic.valerija@gmail.com>
2017-11-08 17:53:05 +0100

    Add gem

---
+++ jekyll-compress-images.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = 'jekyll-compress-images'
+  s.version     = '0.1.5'
+  s.licenses    = ['MIT']
+  s.summary     = "Jekyll plugin for compress/optimize images (jpg, png, gif, svg)"
+  s.description = "Plugin for compress/optimize images (jpg, png, gif, svg). If you are struggle how to install more information you can find here: https://github.com/valerijaspasojevic/jekyll-compress-images"
+  s.authors     = ["Valerija Spasojevic"]
+  s.email       = 'spasojevic.valerija@gmail.com'
+  s.files       = Dir.glob('lib/*')
+  s.homepage    = 'https://rubygems.org/gems/example'
+  s.metadata    = { "source_code_uri" => "https://github.com/valerijaspasojevic/jekyll-compress-images" }
+  s.add_runtime_dependency 'image_optim', '>= 0'
+  s.add_runtime_dependency 'image_optim_pack', '>= 0'
+end
---
+++ lib/jekyll-compress-images.rb
@@ -0,0 +1,48 @@
+require "image_optim"
+require "image_optim_pack"
+require 'yaml'
+
+module Jekyll
+  class CompressImages < Generator
+    safe true
+
+    def generate(site)
+
+      config = YAML::load_file "_config.yml"
+
+      config = config["compress_images"] || {}
+
+      @config = default_options.merge! config
+
+      @image_optim = ImageOptim.new pngout: false, svgo: false, verbose: false
+
+      @last_update = YAML::load_file @config["cache_file"] if File.file? @config["cache_file"]
+      @last_update ||= {}
+
+      Dir.glob(@config["images_path"]) { |image| analyze image }
+
+      File.open(@config["cache_file"], "w") { |file| file.write @last_update.to_yaml }
+    end
+
+    def default_options
+      {
+        "cache_file" => "_compress_images_cache.yml",
+        "images_path" => "assets/img/**/*.{gif,png,jpg,jpeg}",
+      }
+    end
+
+    def analyze(image)
+      if @last_update.has_key? image
+        optimize image if @last_update[image] != File.basename(image)
+      else
+        optimize image
+      end
+    end
+
+    def optimize(image)
+      puts "Optimizing #{image}".green
+      @image_optim.optimize_image! image
+      @last_update[image] = File.basename image
+    end
+  end
+end
